dd20c98a5fa10c9a8a8738a918f08dbb15df8d22
Stefan Dimitrov <hristiyana.dineva@helecloud.com>
2025-07-30 15:55:37 +0300

    Change vnet name

--- network.tf
+++ network.tf
@@ -1,5 +1,5 @@
 resource "azurerm_virtual_network" "vnet" {
-  name                = "main-vnet"
+  name                = "main-vnet1"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   address_space       = ["10.0.0.0/16"]
