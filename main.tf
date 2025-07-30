698c48d1bc4f6cfeacf78b09116bfee863ed920c
Hristiyana Dineva <hristiyana.dineva@helecloud.com>
2025-07-30 15:09:09 +0300

    Add main.tf

---
+++ main.tf
@@ -0,0 +1,98 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "1-a443f218-playground-sandbox"
+  location = "South Central US"
+}
+
+
+resource "azurerm_linux_virtual_machine" "frontend" {
+  name                = "frontend-machine"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.frontend.id ,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+
+  computer_name  = "frontendvm"
+ # custom_data = filebase64("frontend-init.sh")
+}
+
+resource "azurerm_linux_virtual_machine" "backend" {
+  name                = "backend-machine"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.backend.id ,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+  
+  computer_name  = "backendvm"
+ # custom_data = filebase64("backend-init.sh")
+}
+
+resource "azurerm_linux_virtual_machine" "DB" {
+  name                = "DB-machine"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.DB.id ,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+  
+  computer_name  = "DBvm"
+ # custom_data = filebase64("DB-init.sh")
+}
